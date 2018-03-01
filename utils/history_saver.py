2bf6a4219527881ffb576aaf52ff08892bb4b2f1
Tianz <ssstz@outlook.com>
2018-03-01 12:03:40 +0800

    Update history_saver.py

--- utils/history_saver.py
+++ utils/history_saver.py
@@ -1,10 +1,18 @@
 import os
 import matplotlib
+
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 
 
-def plot_history(history, result_dir):
+def plot_history(history, result_dir, save_prefix):
+    """
+    plot training log
+    :param history: training log
+    :param result_dir: save path
+    :param save_prefix: prefix for saved file
+    :return:None
+    """
     plt.plot(history.history['acc'], marker='.')
     plt.plot(history.history['val_acc'], marker='.')
     plt.title('model accuracy')
@@ -12,7 +20,7 @@ def plot_history(history, result_dir):
     plt.ylabel('accuracy')
     plt.grid()
     plt.legend(['acc', 'val_acc'], loc='lower right')
-    plt.savefig(os.path.join(result_dir, 'model_accuracy.png'))
+    plt.savefig(os.path.join(result_dir, '{}_model_accuracy.png'.format(save_prefix)))
     plt.close()
 
     plt.plot(history.history['loss'], marker='.')
@@ -22,18 +30,25 @@ def plot_history(history, result_dir):
     plt.ylabel('loss')
     plt.grid()
     plt.legend(['loss', 'val_loss'], loc='upper right')
-    plt.savefig(os.path.join(result_dir, 'model_loss.png'))
+    plt.savefig(os.path.join(result_dir, '{}_model_loss.png'.format(save_prefix)))
     plt.close()
 
 
-def save_history(history, result_dir):
+def save_history(history, result_dir, save_prefix):
+    """
+    save training log to .txt file
+    :param history: training log
+    :param result_dir: save path
+    :param save_prefix: prefix for saved file
+    :return:None
+    """
     loss = history.history['loss']
     acc = history.history['acc']
     val_loss = history.history['val_loss']
     val_acc = history.history['val_acc']
     nb_epoch = len(acc)
 
-    with open(os.path.join(result_dir, 'result.txt'), 'w') as fp:
+    with open(os.path.join(result_dir, '{}_result.txt'.format(save_prefix)), 'w') as fp:
         fp.write('epoch\tloss\tacc\tval_loss\tval_acc\n')
         for i in range(nb_epoch):
             fp.write('{}\t{}\t{}\t{}\t{}\n'.format(
