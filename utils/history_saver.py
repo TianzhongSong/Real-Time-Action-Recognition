e83d48fe60ee486d71491fcc59c4824933a03944
Tianz <ssstz@outlook.com>
2018-03-01 10:02:11 +0800

    Create history_saver.py

---
+++ utils/history_saver.py
@@ -0,0 +1,41 @@
+import os
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+
+
+def plot_history(history, result_dir):
+    plt.plot(history.history['acc'], marker='.')
+    plt.plot(history.history['val_acc'], marker='.')
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.grid()
+    plt.legend(['acc', 'val_acc'], loc='lower right')
+    plt.savefig(os.path.join(result_dir, 'model_accuracy.png'))
+    plt.close()
+
+    plt.plot(history.history['loss'], marker='.')
+    plt.plot(history.history['val_loss'], marker='.')
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.grid()
+    plt.legend(['loss', 'val_loss'], loc='upper right')
+    plt.savefig(os.path.join(result_dir, 'model_loss.png'))
+    plt.close()
+
+
+def save_history(history, result_dir):
+    loss = history.history['loss']
+    acc = history.history['acc']
+    val_loss = history.history['val_loss']
+    val_acc = history.history['val_acc']
+    nb_epoch = len(acc)
+
+    with open(os.path.join(result_dir, 'result.txt'), 'w') as fp:
+        fp.write('epoch\tloss\tacc\tval_loss\tval_acc\n')
+        for i in range(nb_epoch):
+            fp.write('{}\t{}\t{}\t{}\t{}\n'.format(
+                i, loss[i], acc[i], val_loss[i], val_acc[i]))
+        fp.close()
