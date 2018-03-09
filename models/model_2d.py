8d7bffdb0378fbc3eb19407f49b7896aa46ff72d
Tianz <ssstz@outlook.com>
2018-03-09 13:05:37 +0800

    Create model_2d.py

---
+++ models/model_2d.py
@@ -0,0 +1,30 @@
+from keras.layers import Dense, Dropout, Conv2D, Input, MaxPool2D, Flatten, Activation
+from keras.regularizers import l2
+from keras.models import Model
+
+
+def cnn_2d(input_shape, nb_classes, weight_decay=0.0005):
+   inputs = Input(input_shape)
+   x = Conv2D(16, (3, 3), strides=(2, 2), padding='same',
+              activation='relu', kernel_regularizer=l2(weight_decay))(inputs)
+   x = MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
+   x = Conv2D(32, (3, 3), strides=(1, 1), padding='same',
+              activation='relu', kernel_regularizer=l2(weight_decay))(x)
+   x = MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
+
+   x = Conv2D(32, (3, 3), strides=(1, 1), padding='same',
+              activation='relu', kernel_regularizer=l2(weight_decay))(x)
+
+   x = MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
+
+
+   x = Flatten()(x)
+   x = Dense(128, activation='relu', kernel_regularizer=l2(weight_decay))(x)
+   x = Dropout(0.5)(x)
+   x = Dense(128, activation='relu', kernel_regularizer=l2(weight_decay))(x)
+   x = Dropout(0.5)(x)
+   x = Dense(nb_classes, kernel_regularizer=l2(weight_decay))(x)
+   x = Activation('softmax')(x)
+
+   model = Model(inputs, x)
+   return model
