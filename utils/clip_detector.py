d33662d7997fb3a93e9a4a0f5c8abed0ae1dca94
Tianz <ssstz@outlook.com>
2018-03-25 13:35:57 +0800

    Update and rename ssd_detector.py to clip_detector.py

--- utils/clip_detector.py
+++ utils/clip_detector.py
@@ -3,29 +3,16 @@ import cv2
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 import numpy as np
-from utils.ssd_utils import BBoxUtility
 import copy
+import time
 
 
-def process_image(input_image, ssd_model, empty_count):
+def process_image(input_image, ssd_model, empty_count, bbox_util):
     input_shape = (300, 300, 3)
-    num_classes = 21
-    conf_thresh = 0.4
-    bbox_util = BBoxUtility(num_classes)
-    class_colors = []
-    for i in range(0, num_classes):
-        hue = 255 * i / num_classes
-        col = np.zeros((1, 1, 3)).astype("uint8")
-        col[0][0][0] = hue
-        col[0][0][1] = 128  # Saturation
-        col[0][0][2] = 255  # Value
-        cvcol = cv2.cvtColor(col, cv2.COLOR_HSV2BGR)
-        col = (int(cvcol[0][0][0]), int(cvcol[0][0][1]), int(cvcol[0][0][2]))
-        class_colors.append(col)
-
+    conf_thresh = 0.5
     # Compute aspect ratio of video
-    vidw = 1280
-    vidh = 760
+    vidw = 640
+    vidh = 480
 
     im_size = (input_shape[0], input_shape[1])
     resized = cv2.resize(input_image, im_size)
@@ -56,9 +43,7 @@ def process_image(input_image, ssd_model, empty_count):
         top_xmax = det_xmax[top_indices]
         top_ymax = det_ymax[top_indices]
 
-        if 15 not in top_label_indices:
-            return curl, bbox, empty_count + 1, False
-        else:
+        if 15 in top_label_indices:
             for i in range(top_conf.shape[0]):
                 xmin = int(round((top_xmin[i] * vidw) * 0.9))
                 ymin = int(round((top_ymin[i] * vidh) * 0.9))
@@ -74,11 +59,10 @@ def process_image(input_image, ssd_model, empty_count):
                     bbox = [xmin, ymin, xmax, ymax]
                     frame = copy.deepcopy(input_image)
                     cv2.rectangle(input_image, (xmin, ymin), (xmax, ymax),
-                                  class_colors[class_num], 2)
+                                  (0, 255, 0), 2)
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
                     curl = np.zeros_like(frame, dtype='uint8')
                     curl[ymin:ymax, xmin:xmax, :] = frame[ymin:ymax, xmin:xmax, :]
                     curl = cv2.resize(curl, (112, 112))
-                    return curl, bbox, empty_count, True
-    return curl, bbox, empty_count, False
+    return input_image
