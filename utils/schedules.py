c586ab2220d62c5988793a5bc6ea7ee063be6b1a
Tianz <ssstz@outlook.com>
2018-03-25 13:39:58 +0800

    Update schedules.py

--- utils/schedules.py
+++ utils/schedules.py
@@ -5,10 +5,19 @@ from .callbacks import Step
 
 
 def onetenth_4_8_12(lr):
-    steps = [4, 8, 12]
+    steps = [10, 25, 40]
     lrs = [lr, lr / 10, lr / 100, lr / 1000]
     return Step(steps, lrs)
 
+def onetenth_10_20_30(lr):
+    steps = [15, 25, 35]
+    lrs = [lr, lr / 10, lr / 100, lr / 1000]
+    return Step(steps, lrs)
+
+def onetenth_5_10_15(lr):
+    steps = [5, 10, 15]
+    lrs = [lr, lr / 10, lr / 100, lr / 1000]
+    return Step(steps, lrs)
 
 def onetenth_20_30(lr):
     steps = [20, 30]
