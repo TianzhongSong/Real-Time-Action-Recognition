95255abc80cb8cd18767a2c38136a69abfd398eb
Tianz <ssstz@outlook.com>
2018-03-25 13:35:26 +0800

    Update and rename detector_mode1.py to pose_detector.py

--- utils/pose_detector.py
+++ utils/pose_detector.py
@@ -3,31 +3,17 @@ import cv2
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 import numpy as np
-from utils.ssd_utils import BBoxUtility
 import copy
+from config import *
 
 
-def detect_image(input_image, ssd_model, cnn_2d):
-    action_class = ['stand', 'sit']
+def detect_image(input_image, ssd_model, cnn_2d, bbox_util):
+    pose_class = ['stand', 'sit', 'bend', 'squat']
     input_shape = (300, 300, 3)
-    num_classes = 21
-    conf_thresh = 0.4
-    bbox_util = BBoxUtility(num_classes)
-    class_colors = []
-    for i in range(0, num_classes):
-        hue = 255 * i / num_classes
-        col = np.zeros((1, 1, 3)).astype("uint8")
-        col[0][0][0] = hue
-        col[0][0][1] = 128  # Saturation
-        col[0][0][2] = 255  # Value
-        cvcol = cv2.cvtColor(col, cv2.COLOR_HSV2BGR)
-        col = (int(cvcol[0][0][0]), int(cvcol[0][0][1]), int(cvcol[0][0][2]))
-        class_colors.append(col)
-
+    conf_thresh = 0.5
+    vidw = winWidth
+    vidh = winHeight
     # Compute aspect ratio of video
-    vidw = 1280
-    vidh = 760
-
     im_size = (input_shape[0], input_shape[1])
     resized = cv2.resize(input_image, im_size)
     rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
@@ -58,9 +44,7 @@ def detect_image(input_image, ssd_model, cnn_2d):
         top_xmax = det_xmax[top_indices]
         top_ymax = det_ymax[top_indices]
 
-        if 15 not in top_label_indices:
-            return input_image, action_names
-        else:
+        if 15 in top_label_indices:
             action_names = []
             for i in range(top_conf.shape[0]):
                 xmin = int(round((top_xmin[i] * vidw) * 0.9))
@@ -76,23 +60,22 @@ def detect_image(input_image, ssd_model, cnn_2d):
                 if class_num == 15:
                     frame = copy.deepcopy(input_image)
                     cv2.rectangle(input_image, (xmin, ymin), (xmax, ymax),
-                                  class_colors[class_num], 2)
+                                  (0, 255, 0), 2)
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    # crop = frame[ymin:ymax, xmin:xmax, :]
-                    curl = np.zeros_like(frame, dtype='uint8')
-                    curl[ymin:ymax, xmin:xmax, :] = frame[ymin:ymax, xmin:xmax, :]
-                    crop = cv2.resize(curl, (64, 64))
+                    crop = frame[ymin:ymax, xmin:xmax, :]
+                    # crop = np.zeros_like(frame, dtype='uint8')
+                    # crop[ymin:ymax, xmin:xmax, :] = frame[ymin:ymax, xmin:xmax, :]
+                    crop = cv2.resize(crop, (cnn2d_ImW, cnn2d_ImH))
                     image_data.append(crop)
                     bboxs.append([xmin, ymin])
             if len(image_data) > 0:
                 image_data = np.array(image_data).astype(np.float32)
-                image_data /= 255
+                image_data /= 255.
                 pose_results = cnn_2d.predict_on_batch(image_data)
                 for j in range(len(image_data)):
-                    action_name = action_class[np.argmax(pose_results[j])]
+                    action_name = pose_class[np.argmax(pose_results[j])]
                     action_names.append(action_name)
                     cv2.putText(input_image, action_name + ' %.2f' % max(pose_results[j]),
                                 (bboxs[j][0] + 20, bboxs[j][1] + 20),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-            return input_image, action_names
     return input_image, action_names
