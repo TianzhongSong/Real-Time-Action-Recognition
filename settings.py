6e943334921038ef3cee6cdbea186ac139a393e2
Tianz <ssstz@outlook.com>
2018-05-03 17:49:17 +0800

    Update and rename config.py to settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,18 @@
+import numpy as np
+
 clip_length = 16
 ssd_input_shape = (300, 300, 3)
-c3d_input_shape = (112, 112, clip_length, 3)
+input_shape_3d = (112, 112, clip_length, 3)
+mode_3d = 'mask'
+
 cnn2d_ImW = 64
 cnn2d_ImH = 64
 cnn_2d_input_shape = (cnn2d_ImH, cnn2d_ImW, 3)
-winWidth = 640
-winHeight = 480
-mode = 'crop'
+winWidth = 1280
+winHeight = 720
+mode_2d = 'crop'
 action_classes = ['standing', 'walking', 'sitting']
 pose_classes = ['stand', 'sit', 'bend', 'squat']
+colours = np.random.rand(32, 3) * 255
+sort_max_age = 5
+sort_min_hit = 3
