fe04e1d2828bfc36a777f60add8bf91b2abe38f1
Tianz <ssstz@outlook.com>
2018-06-21 19:39:05 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,14 @@
 import numpy as np
 
-clip_length = 16
-ssd_input_shape = (300, 300, 3)
-input_shape_3d = (112, 112, clip_length, 3)
-mode_3d = 'mask'
+# clip length
+L = 15
+# winWidth = 1280
+winWidth = 640
+# winHeight = 720
+winHeight = 480
+move_status = ['', 'stand', 'sit', 'walk', 'walk close', 'walk away', 'sit down', 'stand up']
 
-cnn2d_ImW = 64
-cnn2d_ImH = 64
-cnn_2d_input_shape = (cnn2d_ImH, cnn2d_ImW, 3)
-winWidth = 1280
-winHeight = 720
-mode_2d = 'crop'
-action_classes = ['standing', 'walking', 'sitting']
-pose_classes = ['stand', 'sit', 'bend', 'squat']
-colours = np.random.rand(32, 3) * 255
-sort_max_age = 5
+c = np.random.rand(32, 3) * 255
+sort_max_age = 20
 sort_min_hit = 3
+
