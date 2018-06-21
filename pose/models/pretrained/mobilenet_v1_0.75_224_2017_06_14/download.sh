0c37aa58f5dda41c4454081a41c98f59c429211b
Tianz <ssstz@outlook.com>
2018-06-21 19:46:42 +0800

    Create download.sh

---
+++ pose/models/pretrained/mobilenet_v1_0.75_224_2017_06_14/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://www.mediafire.com/file/kibz0x9e7h11ueb/mobilenet_v1_0.75_224.ckpt.data-00000-of-00001
+wget http://www.mediafire.com/file/t8909eaikvc6ea2/mobilenet_v1_0.75_224.ckpt.index
+wget http://www.mediafire.com/file/6jjnbn1aged614x/mobilenet_v1_0.75_224.ckpt.meta
